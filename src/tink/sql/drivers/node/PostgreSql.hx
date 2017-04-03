b43b1d5787f0672b3971fe01c6a7a5615831bfb3
Kevin Leung <kevinresol@gmail.com>
2017-04-04 00:20:44 +0800

    insert() kind of working

--- src/tink/sql/drivers/node/PostgreSql.hx
+++ src/tink/sql/drivers/node/PostgreSql.hx
@@ -10,6 +10,7 @@ import tink.sql.Info;
 import tink.sql.types.*;
 
 using tink.CoreApi;
+using StringTools;
 
 class PostgreSql implements Driver {
 	var settings:Dynamic;
@@ -35,23 +36,49 @@ class PostgreSqlConnection<Db:DatabaseInfo> implements Connection<Db> implements
 		this.client = client;
 	}
 	
-	public function value(v:Any):String
-		return PgEscape.literal(v);
+	public function value(v:Any):String {
+		return 
+			if(v == null) 'NULL';
+			else if(Std.is(v, Bool)) v ? 'true' : 'false';
+			else if(Std.is(v, Float)) Std.string(v);
+			else if(Std.is(v, Date)) throw 'not implemented';
+			else if(Std.is(v, haxe.io.Bytes)) throw 'not implemented'; // as BLOB
+			else if(Std.is(v, Array)) throw 'not implemented';
+			else if(Std.is(v, String)) "'" + v.replace("'", "''") + "'";
+			else throw 'Invalid value $v';
+	}
+		
 		
 	public function ident(s:String):String
-		return PgEscape.ident(s);
+		return '"$s"';
 	
 	public function selectAll<A:{}>(t:Target<A, Db>, ?c:Condition, ?limit:Limit):Stream<A> {
 		var accumulator = new Accumulator<A>();
 		var query = client.query(Format.selectAll(t, c, this));
 		query.on('row', function(row:A) accumulator.yield(Data(row)));
-		query.on('error', function(e:js.Error) accumulator.yield(Fail(Error.withData(e.message, e))));
+		query.on('error', function(e) accumulator.yield(Fail(toError(e))));
 		query.on('end', function() accumulator.yield(End));
 		return accumulator;
 	}
 	
+	function toError<A>(error:js.Error):Error
+		return Error.withData(error.message, error);//TODO: give more information
+	
 	public function insert<Row:{}>(table:TableInfo<Row>, items:Array<Insert<Row>>):Surprise<Id<Row>, Error> {
-		throw 'not implemented';
+		return Future.async(function (cb) {
+			client.query(
+				'INSERT INTO "auto2" ("foo") VALUES (\'2017-04-03 23:57:34\') RETURNING id',
+				function (error, result) {
+					cb(switch [error, result] {
+						case [null, _]:
+							trace(result);
+							trace(result._parsers[0]());
+							Success(new Id(null));
+						case [e, _]: Failure(toError(e));
+					});
+				}
+			);
+		});
 	}
 	
 	public function update<Row:{}>(table:TableInfo<Row>, ?c:Condition, ?max:Int, update:Update<Row>):Surprise<{ rowsAffected: Int }, Error> {
